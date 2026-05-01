13689d748e7ca1629ac1b81020051c4f40122c1e
brayan <marcosbrayan03@gmail.com>
2026-05-01 02:07:33 +0200

    refactor(ipc): remove unused folder handlers registration

--- src/main/ipc/index.js
+++ src/main/ipc/index.js
@@ -1,12 +1,10 @@
 const { registerNoteHandlers } = require("./noteHandlers.js");
-const { registerFolderHandlers } = require("./folderHandlers.js");
 const { registerWindowHandlers } = require("./windowHandlers.js");
 const { registerUpdateHandlers } = require("./updateHandlers.js");
 const { registerExportHandlers } = require("./exportHandlers.js");
 
 function registerAllHandlers() {
   registerNoteHandlers();
-  registerFolderHandlers();
   registerWindowHandlers();
   registerUpdateHandlers();
   registerExportHandlers();
